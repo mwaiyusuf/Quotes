da1e105ecef887ff42562d328b4ea8a439e6c263
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-22 17:34:05 +0300

    quote class

---
+++ src/app/Quote.spec.ts
@@ -0,0 +1,7 @@
+import { Quote } from './Quote';
+
+describe('Quotes', () => {
+  it('should create an instance', () => {
+    expect(new Quotes()).toBeTruthy();
+  });
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// import { Quote } from '@angular/compiler';
+import { Quote } from './Quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes pro!!';
+  quotes =[
+    new Quote(1,'Never give up bro'),
+    new Quote(2, 'God is with you')
+  ];
+   
 }
