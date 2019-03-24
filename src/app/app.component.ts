3cf17d8e0d6dff000d4f7872c18facedb6184841
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-24 22:13:06 +0300

    down voting and upvoting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { Quote } from './quote';
 
 })
 export class AppComponent {
-  title = 'Quotes pro!!';
-  quotes =[
-    new Quote(1,'Never give up bro',',',new Date(2017,3,24)),
-    new Quote(2, 'God is with you',',',new Date(2017,3,24)),
-    new Quote(3,'Never give up bro',',',new Date(2017,3,24)),
-    new Quote(4,'Never give up bro',',',new Date(2017,3,24)),
-    new Quote(5,'Never give up bro',',',new Date(2017,3,24)),
-    new Quote(6,'Never give up bro',',',new Date(2017,3,24)),
-  ]
+  // title = 'Quotes pro!!';
+  // quotes =[
+  //   new Quote(1,'Never give up bro',',',new Date(2017,3,24)),
+  //   new Quote(2, 'God is with you',',',new Date(2017,3,24)),
+  //   new Quote(3,'Never give up bro',',',new Date(2017,3,24)),
+  //   new Quote(4,'Never give up bro',',',new Date(2017,3,24)),
+  //   new Quote(5,'Never give up bro',',',new Date(2017,3,24)),
+  //   new Quote(6,'Never give up bro',',',new Date(2017,3,24)),
+  // ]
    
 }
--- src/app/up-down-vote/up-down-vote.component.ts
+++ src/app/up-down-vote/up-down-vote.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter} from '@angular/core';
+import {Quote} from '../quote'
 @Component({
   selector: 'app-up-down-vote',
   templateUrl: './up-down-vote.component.html',
   styleUrls: ['./up-down-vote.component.css']
 })
 export class UpDownVoteComponent implements OnInit {
-
+@Output() isComplete = new EventEmitter<boolean>();
+uvotes = 0 ;
+dvotes = 0 ;
+upVote(){
+  this.uvotes = this.uvotes + 1;
+}
+downVote(){
+  this.dvotes + 1;
+}
   constructor() { }
 
   ngOnInit() {
