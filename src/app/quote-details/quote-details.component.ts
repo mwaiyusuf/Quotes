d5f15dad6a840e19caadb02a1d7c9a16b3ea80f6
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-24 14:34:04 +0300

    output of devote

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input, Output} from '@angular/core';
+import {Quote} from '../quote'
+import { EventEmitter } from 'protractor';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-
+@Input() quote:Quote;
+@Output() isComplete= new EventEmitter<boolean>();
+goalComplete(complete:boolean){
+  this.isComplete.emit(complete);
+}
   constructor() { }
 
   ngOnInit() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Quote } from '../Quote'
-@Component({
-  selector: 'app-quote-details',
-  templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
-})
-export class QuoteDetailComponent implements OnInit {
- @Input() quote:Quote;
-  constructor() { }
+export class QuoteComponent implements OnInit {
+  quotes = [
+      new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
+      new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
+      new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
+      new Quote(4, 'Get Dog Food','Pupper likes expensive sancks'),
+      new Quote(5, 'Solve math homework','Damn Math'),
+      new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
 
-  ngOnInit() {
-  }
+  ]
+  completeQuote(isComplete,index){
+    if (isComplete){
+        this.quotes.splice(index,1);
+        }
+        }
 
-}
+ 
+   
+
+ 
