a9437fb1f38c15ea8f325fdb38a839860663dc7e
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-24 23:57:57 +0300

    form styling

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { Quote } from '../Quote'
 @Component ({
     selector: 'app-quote-form',
-    templateUrl : '.quote-form.component.html',
+    templateUrl : './quote-form.component.html',
     // styleUrls : '.quote-form.component.css'
 })
 export class QuoteFormComponent implements OnInit {
