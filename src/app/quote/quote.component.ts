be7ac303e7f9a7951bcaa580a2e7d2d5394e6eed
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-24 15:31:45 +0300

    delete function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,20 +1,24 @@
 export class QuoteComponent implements OnInit {
   quotes = [
-      new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
-      new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-      new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-      new Quote(4, 'Get Dog Food','Pupper likes expensive sancks'),
-      new Quote(5, 'Solve math homework','Damn Math'),
-      new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+      new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2017,3,24)),
+      new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2017,3,24)),
+      new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon',new Date(2017,3,24)),
+      new Quote(4, 'Get Dog Food','Pupper likes expensive sancks',new Date(2017,3,24)),
+      new Quote(5, 'Solve math homework','Damn Math',new Date(2017,3,24)),
+      new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2017,3,24)),
 
   ]
-  completeQuote(isComplete,index){
+  deleteQuote(isComplete,index){
     if (isComplete){
+      let toDelete=confirm(Are you sure you wanna delete ${this.quotes[index].name})
+      if(toDelete){
         this.quotes.splice(index,1);
+      }
+        
         }
         }
-
+      }
  
-   
+      
 
  
