f5c77bf6cd6ef583fc5a220ed011ededbdc2942a
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-24 14:30:44 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,11 @@ export class AppComponent {
   title = 'Quotes pro!!';
   quotes =[
     new Quote(1,'Never give up bro'),
-    new Quote(2, 'God is with you')
-  ];
+    new Quote(2, 'God is with you'),
+    new Quote(3,'Never give up bro'),
+    new Quote(4,'Never give up bro'),
+    new Quote(5,'Never give up bro'),
+    new Quote(6,'Never give up bro'),
+  ]
    
 }
