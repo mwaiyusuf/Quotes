c24a151b1584ffe3ddb38cf473a6b4f0cfe55f42
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-25 17:55:30 +0300

    Added Styles to make my page suitable

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,8 +8,8 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes = [
-    new Quote(1, "One man's pain is another man pleasure.", "Clinton",0, 0, "Thomas"),
-    new Quote(2, "One man's meat is another man's poison.", "J-cole", 0, 0,"Thomas")
+    new Quote(1, "I have the keys to the streets", "Jemo",0, 0, "Kevin Hart"),
+    new Quote(2, "Look back at it", "Nelson", 0, 0,"Harry kane")
   ]
   toggleDetails(index) {
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
